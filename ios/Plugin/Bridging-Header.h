0f8e2b7a36d7442ff72c7fae5763147160d14c4c
himanushi <himanushi@users.noreply.github.com>
2022-03-24 21:46:36 +0900

    fix

---
+++ ios/Plugin/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Plugin
+//
+//  Created by himanushi on 2022/03/24.
+//  Copyright © 2022 Max Lynch. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <GoogleAnalytics/GAI.h>
+#import <GoogleAnalytics/GAIDictionaryBuilder.h>
+
+#endif /* Bridging_Header_h */
